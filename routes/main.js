a37e8e7e80d533c4df508acadbd0b2cfa2b4c2d4
B4MB4N9-ID <68556996+B4MBANG-94NT3NG@users.noreply.github.com>
2021-03-18 19:27:43 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + 'views/open.html')
+})
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/doc.html')
 })
 
 router.get('/about', (req, res) => {
